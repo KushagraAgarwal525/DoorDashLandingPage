db5138e609614bf06bfd2e2c08a1811b0f093c97
KushagraAgarwal525 <kushagraagarwal525.ka@gmail.com>
2022-05-03 20:33:52 +0530

    Add responsive hero

---
+++ src/Hero.js
@@ -0,0 +1,26 @@
+import Logo from './Logo';
+
+function Hero() {
+    return (
+        <div className="hero">
+            <Logo />
+            <div className="hero-cta-button-container">
+                <div className="hero-cta-buttons">
+                    <div className="hero-cta-button">
+                        <a href="https://www.doordash.com/">Sign In</a>
+                    </div>
+                    <div className="hero-cta-button light">
+                        <a href="https://www.doordash.com/">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+            <div className="hero-catchphrase-container">
+                <div className="hero-catchphrase">
+                    Restraunts and more, delivered to your door
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
