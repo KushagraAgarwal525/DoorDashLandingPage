220c61a229a629b2e576317ed9158212add83183
KushagraAgarwal525 <kushagraagarwal525.ka@gmail.com>
2022-05-05 14:45:34 +0530

    Make changes for better responsive design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { useEffect } from 'react';
 import Hero from './Hero';
 
 function App() {
-    useEffect(() => {document.title = "DoorDash Food Delivery & Takeout - From Restaurants Near You"}, []);
     return (
         <div className="App">
-            <Hero /> 
+            <Hero />
         </div>
     );
 }
